ee56c2b1be66651103ea734c1941b2fd1faec01c
Axwabo <axwabo@gmail.com>
2022-07-15 10:56:50 +0200

    serialization fix

--- Axwabo.Helpers/Config/MapPointByName.cs
+++ Axwabo.Helpers/Config/MapPointByName.cs
@@ -9,7 +9,7 @@ namespace Axwabo.Helpers.Config {
     /// </summary>
     /// <seealso cref="ConfigHelper.GetRoomName"/>
     [Serializable]
-    public readonly struct MapPointByName {
+    public struct MapPointByName {
 
         /// <summary>
         /// An empty config object, representing no rooms.
@@ -21,17 +21,17 @@ namespace Axwabo.Helpers.Config {
         /// <summary>
         /// The name of the room.
         /// </summary>
-        public readonly string RoomName;
+        public string RoomName { get; set; }
 
         /// <summary>
         /// World-space position offset to apply to the room.
         /// </summary>
-        public readonly Vector3 PositionOffset;
+        public Vector3 PositionOffset { get; set; }
 
         /// <summary>
         /// Rotational offset to the room.
         /// </summary>
-        public readonly Quaternion RotationOffset;
+        public SerializedRotation RotationOffset { get; set; }
 
         #endregion
 
@@ -41,7 +41,7 @@ namespace Axwabo.Helpers.Config {
         /// Initializes a new instance without any offset.
         /// </summary>
         /// <param name="name">The name of the room.</param>
-        public MapPointByName(string name) : this(name, Vector3.zero, Quaternion.identity) {
+        public MapPointByName(string name) : this(name, Vector3.zero, SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -49,7 +49,7 @@ namespace Axwabo.Helpers.Config {
         /// </summary>
         /// <param name="name">The name of the room.</param>
         /// <param name="positionOffset">A world-space position offset.</param>
-        public MapPointByName(string name, Vector3 positionOffset) : this(name, positionOffset, Quaternion.identity) {
+        public MapPointByName(string name, Vector3 positionOffset) : this(name, positionOffset, SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -59,7 +59,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="x">Offset on the X axis.</param>
         /// <param name="y">Offset on the Y axis.</param>
         /// <param name="z">Offset on the Z axis.</param>
-        public MapPointByName(string name, float x, float y, float z) : this(name, new Vector3(x, y, z), Quaternion.identity) {
+        public MapPointByName(string name, float x, float y, float z) : this(name, new Vector3(x, y, z), SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -68,7 +68,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="name">The name of the room.</param>
         /// <param name="positionOffset">A world-space position offset.</param>
         /// <param name="rotationOffset">A rotational offset.</param>
-        public MapPointByName(string name, Vector3 positionOffset, Quaternion rotationOffset) {
+        public MapPointByName(string name, Vector3 positionOffset, SerializedRotation rotationOffset) {
             RoomName = name;
             PositionOffset = positionOffset;
             RotationOffset = rotationOffset;
@@ -82,22 +82,22 @@ namespace Axwabo.Helpers.Config {
         /// If the object has been initialized using a constructor.
         /// </summary>
         /// <remarks>This does not check if the room exists, unlike <see cref="MapPointByRoomType.IsValid">MapPointByRoomType</see> does.</remarks>
-        public bool IsValid => !string.IsNullOrEmpty(RoomName);
+        public bool IsValid() => !string.IsNullOrEmpty(RoomName);
 
         /// <summary>
         /// Gets the room component for the given <see cref="Type">room type</see>.
         /// </summary>
-        public Room RoomObject => ConfigHelper.GetRoomByRoomName(RoomName);
+        public Room RoomObject() => ConfigHelper.GetRoomByRoomName(RoomName);
 
         /// <summary>
         /// Gets the transform of the room object.
         /// </summary>
-        public Transform RoomTransform => RoomObject.SafeGetTransform();
+        public Transform RoomTransform() => RoomObject().SafeGetTransform();
 
         /// <summary>
         /// Gets the world-space position and rotation by applying the offset to the room.
         /// </summary>
-        public Pose WorldPose => TryGetWorldTransform(out var pos, out var rot) ? new Pose(pos, rot) : Pose.identity;
+        public Pose WorldPose() => TryGetWorldTransform(out var pos, out var rot) ? new Pose(pos, rot) : Pose.identity;
 
         /// <summary>
         /// Attempts to get the world-space position and rotation by applying the offset to the room.
@@ -105,7 +105,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="position">The world-space position to store.</param>
         /// <param name="rotation">The world-space rotation to store.</param>
         /// <returns>If the room is not null.</returns>
-        public bool TryGetWorldTransform(out Vector3 position, out Quaternion rotation) => RoomTransform.TryTransformOffset(PositionOffset, RotationOffset, out position, out rotation);
+        public bool TryGetWorldTransform(out Vector3 position, out Quaternion rotation) => RoomTransform().TryTransformOffset(PositionOffset, RotationOffset, out position, out rotation);
 
         #endregion
 
--- Axwabo.Helpers/Config/MapPointByRoomType.cs
+++ Axwabo.Helpers/Config/MapPointByRoomType.cs
@@ -10,7 +10,7 @@ namespace Axwabo.Helpers.Config {
     /// </summary>
     /// <seealso cref="ConfigHelper.GetRoomName"/>
     [Serializable]
-    public readonly struct MapPointByRoomType {
+    public struct MapPointByRoomType {
 
         /// <summary>
         /// An empty config object, representing no rooms.
@@ -20,19 +20,19 @@ namespace Axwabo.Helpers.Config {
         #region Info
 
         /// <summary>
-        /// The type of the room.
+        /// The type of thea|ad room.
         /// </summary>
-        public readonly RoomType Type;
+        public RoomType Type { get; set; }
 
         /// <summary>
         /// World-space position offset to apply to the room.
         /// </summary>
-        public readonly Vector3 PositionOffset;
+        public Vector3 PositionOffset { get; set; }
 
         /// <summary>
         /// Rotational offset to the room.
         /// </summary>
-        public readonly Quaternion RotationOffset;
+        public SerializedRotation RotationOffset { get; set; }
 
         #endregion
 
@@ -42,7 +42,7 @@ namespace Axwabo.Helpers.Config {
         /// Initializes a new instance without any offset.
         /// </summary>
         /// <param name="type">The type of the room.</param>
-        public MapPointByRoomType(RoomType type) : this(type, Vector3.zero, Quaternion.identity) {
+        public MapPointByRoomType(RoomType type) : this(type, Vector3.zero, SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -50,7 +50,7 @@ namespace Axwabo.Helpers.Config {
         /// </summary>
         /// <param name="type">The type of the room.</param>
         /// <param name="positionOffset">A world-space position offset.</param>
-        public MapPointByRoomType(RoomType type, Vector3 positionOffset) : this(type, positionOffset, Quaternion.identity) {
+        public MapPointByRoomType(RoomType type, Vector3 positionOffset) : this(type, positionOffset, SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -60,7 +60,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="x">Offset on the X axis.</param>
         /// <param name="y">Offset on the Y axis.</param>
         /// <param name="z">Offset on the Z axis.</param>
-        public MapPointByRoomType(RoomType type, float x = 0, float y = 0, float z = 0) : this(type, new Vector3(x, y, z), Quaternion.identity) {
+        public MapPointByRoomType(RoomType type, float x = 0, float y = 0, float z = 0) : this(type, new Vector3(x, y, z), SerializedRotation.Identity) {
         }
 
         /// <summary>
@@ -69,7 +69,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="type">The type of the room.</param>
         /// <param name="positionOffset">A world-space position offset.</param>
         /// <param name="rotationOffset">A rotational offset.</param>
-        public MapPointByRoomType(RoomType type, Vector3 positionOffset, Quaternion rotationOffset) {
+        public MapPointByRoomType(RoomType type, Vector3 positionOffset, SerializedRotation rotationOffset) {
             Type = type;
             PositionOffset = positionOffset;
             RotationOffset = rotationOffset;
@@ -82,22 +82,22 @@ namespace Axwabo.Helpers.Config {
         /// <summary>
         /// If the object has been initialized using a constructor.
         /// </summary>
-        public bool IsValid => Type != RoomType.Unknown;
+        public bool IsValid() => Type != RoomType.Unknown;
 
         /// <summary>
         /// Gets the room component for the given <see cref="Type">room type</see>.
         /// </summary>
-        public Room RoomObject => Type == RoomType.Unknown ? null : Room.Get(Type);
+        public Room RoomObject() => Type == RoomType.Unknown ? null : Room.Get(Type);
 
         /// <summary>
         /// Gets the transform of the room object.
         /// </summary>
-        public Transform RoomTransform => RoomObject.SafeGetTransform();
+        public Transform RoomTransform() => RoomObject().SafeGetTransform();
 
         /// <summary>
         /// Gets the world-space position and rotation by applying the offset to the room.
         /// </summary>
-        public Pose WorldPose => TryGetWorldTransform(out var pos, out var rot) ? new Pose(pos, rot) : Pose.identity;
+        public Pose WorldPose() => TryGetWorldTransform(out var pos, out var rot) ? new Pose(pos, rot) : Pose.identity;
 
         /// <summary>
         /// Attempts to get the world-space position and rotation by applying the offset to the room.
@@ -105,7 +105,7 @@ namespace Axwabo.Helpers.Config {
         /// <param name="position">The world-space position to store.</param>
         /// <param name="rotation">The world-space rotation to store.</param>
         /// <returns>If the room is not null.</returns>
-        public bool TryGetWorldTransform(out Vector3 position, out Quaternion rotation) => RoomTransform.TryTransformOffset(PositionOffset, RotationOffset, out position, out rotation);
+        public bool TryGetWorldTransform(out Vector3 position, out Quaternion rotation) => RoomTransform().TryTransformOffset(PositionOffset, RotationOffset, out position, out rotation);
 
         #endregion
 
