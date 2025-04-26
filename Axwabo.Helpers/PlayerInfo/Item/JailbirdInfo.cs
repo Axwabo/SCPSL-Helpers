52330f4b28b67ccd88a1922b3cf5eb2a9028f441
Axwabo <axwabo@gmail.com>
2025-04-26 20:35:10 +0200

    XML doc fix

--- Axwabo.Helpers/PlayerInfo/Item/JailbirdInfo.cs
+++ Axwabo.Helpers/PlayerInfo/Item/JailbirdInfo.cs
@@ -25,7 +25,7 @@ public class JailbirdInfo : ItemInfoBase
     public static bool IsJailbird(ItemBase item) => item is JailbirdItem;
 
     /// <summary>
-    /// Creates a new <see cref="FirearmInfo"/> instance.
+    /// Creates a new <see cref="JailbirdInfo"/> instance.
     /// </summary>
     /// <param name="meleeDamage">The amount of total melee damage dealt-</param>
     /// <param name="charges">The amount of charges used.</param>
